4557becefa9f9276b9550fd963900fcd870087c9
Profiidev <92174452+Profiidev@users.noreply.github.com>
2024-04-10 22:54:21 +0200

    Started TitleBar

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,25 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
+  import Titlebar from "./lib/components/main/Titlebar.svelte";
 </script>
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div class="row">
-    <Greet />
-  </div>
-
-
-</main>
+<div class="app">
+  <header>
+    <Titlebar />
+  </header>
+  <main class="container">
+  </main>
+</div>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
+  :global(:root) {
+    --color-accent: #E6B450;
+    --color-primary: #C5C5C5;
+    --color-secondary: #383D45;
+    --color-background: #0B0E14;
   }
 
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
+  .app {
+    height: 100%;
+    background-color: var(--color-background);
   }
 </style>
---
+++ src/lib/components/main/Titlebar.svelte
@@ -0,0 +1,54 @@
+<script type="ts">
+  import { Window } from "@tauri-apps/api/window";
+
+  const appWindow = new Window("main");
+</script>
+
+<div class="titlebar">
+  <div data-tauri-drag-region class="space"></div>
+  <button class="titlebar-button" id="titlebar-minimize" on:click={appWindow.minimize}>
+    <img src="icons/action/minimize.svg" alt="minimize" style="scale: 1.1;" />
+  </button>
+  <button class="titlebar-button" id="titlebar-maximize" on:click={appWindow.toggleMaximize}>
+    <img src="icons/action/maximize.svg" alt="maximize" />
+  </button>
+  <button class="titlebar-button" id="titlebar-close" on:click={appWindow.close}>
+    <img src="icons/action/close.svg" alt="close" style="scale: 1.1;" />
+  </button>
+</div>
+
+<style>
+  .titlebar {
+    height: 35px;
+    user-select: none;
+    display: flex;
+    justify-content: flex-end;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+  }
+  .space {
+    flex: 1;
+  }
+  .titlebar-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    width: 35px;
+    height: 35px;
+    user-select: none;
+    -webkit-user-select: none;
+    border: none;
+    background: transparent;
+    padding: 0;
+  }
+  .titlebar-button:hover {
+    background: var(--color-secondary);
+  }
+  .titlebar-button img {
+    width: 50%;
+    height: 50%;
+    object-fit: contain;
+  }
+</style>
